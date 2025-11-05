a2ea426e83b7bf3e258113c9cfc7da934413ebdb
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2025-11-04 23:26:19 -0600

    WeatherCardupdates

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,10 @@
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75 &deg;</p>
+      <img src="" alt="" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
