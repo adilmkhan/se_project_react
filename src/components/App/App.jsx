a810368bc9d72c93b639e049aec96fe0b298c833
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2025-11-06 23:23:36 -0600

    ModalWithForm_InitialUpdates

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header.jsx";
 import Main from "../Main/Main.jsx";
+import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
+  const [activeModal, setActiveModal] = useState({});
   return (
     <div className="page">
       <div className="page__content">
-        <Header />
+        <Header activeModal={activeModal} />
         <Main weatherData={weatherData} />
       </div>
+      <ModalWithForm />
     </div>
   );
 }
--- src/components/ItemCard/ItemCard.jsx
+++ src/components/ItemCard/ItemCard.jsx
@@ -2,10 +2,10 @@ import "./ItemCard.css";
 
 function ItemCard({ item }) {
   return (
-    <>
+    <li className="cards__item">
       <h2 className="cards__image-description">{item.name}</h2>
       <img src={item.link} className="cards__image" alt="cards_image" />
-    </>
+    </li>
   );
 }
 
