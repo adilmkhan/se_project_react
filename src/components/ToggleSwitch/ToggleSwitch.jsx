4552a93e3bd05ebf604119ea964ce25577235a09
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2025-11-17 22:09:02 -0600

    toggleSwitch_styles

--- src/components/ToggleSwitch/ToggleSwitch.jsx
+++ src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -1,5 +1,12 @@
 import "./ToggleSwitch.css";
 
 export default function ToggleSwitch() {
-  return;
+  return (
+    <label className="toggle-switch">
+      <input type="checkbox" className="toggle-switch__checkbox" />
+      <span className="toggle-switch__circle"></span>
+      <span className="toggle-switch__f">F</span>
+      <span className="toggle-switch__c">C</span>
+    </label>
+  );
 }
