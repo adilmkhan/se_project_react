dbbd9c6ff1d902b1f39735686fca897be69af9a9
Adil Khan <adilkhan@Adils-MacBook-Air.local>
2025-11-22 18:27:03 -0600

    NaigationLinks_initialCommit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import "./Header.css";
 import logo from "../../assets/logo.svg";
 import avatar from "../../assets/avatar.png";
 import ToggleSwitch from "../ToggleSwitch/ToggleSwitch";
+import { NavLink } from "react-router-dom";
 
 function Header({ handleAddClick, weatherData }) {
   const currentDate = new Date().toLocaleString("default", {
@@ -10,7 +11,9 @@ function Header({ handleAddClick, weatherData }) {
   });
   return (
     <header className="header">
-      <img className="header__logo" src={logo} alt="WTWR logo" />
+      <NavLink to="/">
+        <img className="header__logo" src={logo} alt="WTWR logo" />
+      </NavLink>
       <p className="header__data-and-location">
         {currentDate}, {weatherData.city}
       </p>
@@ -23,8 +26,13 @@ function Header({ handleAddClick, weatherData }) {
         + Add Clothes
       </button>
       <div className="header__user-container">
-        <p className="header__username">Terrence Tegegne</p>
-        <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        <NavLink to="/profile" className="header__username">
+          {" "}
+          Terrence Tegegne
+        </NavLink>
+        <NavLink to="/profile">
+          <img src={avatar} alt="Terrence Tegegne" className="header__avatar" />
+        </NavLink>
       </div>
     </header>
   );
